2500e52037e17fcd8c897e2e675d4f6391532f31
Netrapal singh <126900083+netra-tech3@users.noreply.github.com>
2024-05-23 14:16:44 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
  export const connectDB = async () =>
     {
-        await mongoose.connect('mongodb+srv://netrapalsingh7256:yuvrajsingh2007@cluster0.txaqgvi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(()=>
+        await mongoose.connect('//your database').then(()=>
             console.log("DB connected")
         );
     }
